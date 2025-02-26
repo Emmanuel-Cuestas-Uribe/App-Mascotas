399c66ba5d13022d5fe10c3303a447ab26c94bfe
Emmanuel-Cuestas-Uribe <al222210660@gmail.com>
2025-02-26 12:51:42 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
   return (
     <div className="container">
-      <h1>🐾 Lista de Mascotas 🐾</h1>
+      <h1>🐾 Lista de Mascotas QA 🐾</h1>
 
       {/* Buscador */}
       <div className="search-container">
